283eee15eaeeb110dbb74bb7dda4f82fed221393
yuzuki517 <sgwr2532@outlook.com>
2026-01-26 01:14:38 +0900

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     content = models.TextField()
+    image = models.ImageField(upload_to="posts/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
