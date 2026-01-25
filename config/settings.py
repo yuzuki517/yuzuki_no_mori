482fd1d412fb0819204c2a72ab1e44505b57a2c5
yuzuki517 <sgwr2532@outlook.com>
2026-01-25 22:46:17 +0900

    20260125_2246

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
-ADMIN_URL = os.getenv("ADMIN_URL")
+ADMIN_URL = os.getenv("ADMIN_URL") or "admin/"
 
 # Application definition
 
