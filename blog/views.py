1f536664e9ce028b4e7eaccf3ab96ccd885fc9d0
yuzuki517 <sgwr2532@outlook.com>
2026-01-27 01:11:18 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -9,8 +9,9 @@ def index(request):
 
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    body = post.body or "" # None対策
     post.body_html = markdown.markdown(
-        post.body,
+        body,
         extensions=["fenced_code", "codehilite"]
     )
     return render(request, "blog/detail.html", {"post": post})
