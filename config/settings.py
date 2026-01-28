555ac0e4bb06e32f58ec77bcdecd5b95dc48a7cc
yuzuki517 <sgwr2532@outlook.com>
2026-01-29 01:40:07 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
