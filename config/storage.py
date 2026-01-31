437c61ee4a3aabd4a3134f911513de1da036100c
yuzuki517 <sgwr2532@outlook.com>
2026-02-01 02:55:32 +0900

    20260201

--- config/storage.py
+++ config/storage.py
@@ -4,9 +4,9 @@ from storages.backends.azure_storage import AzureStorage
 class AzureStaticStorage(AzureStorage):
     account_name = os.getenv("AZURE_ACCOUNT_NAME")
     account_key = os.getenv("AZURE_ACCOUNT_KEY")
-    azure_container_name = os.getenv("AZURE_CONTAINER_STATIC")
+    azure_container = os.getenv("AZURE_CONTAINER_STATIC")
 
 class AzureMediaStorage(AzureStorage):
     account_name = os.getenv("AZURE_ACCOUNT_NAME")
     account_key = os.getenv("AZURE_ACCOUNT_KEY")
-    azure_container_name = os.getenv("AZURE_CONTAINER_MEDIA")
+    azure_container = os.getenv("AZURE_CONTAINER_MEDIA")
