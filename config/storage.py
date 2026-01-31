996caa9f2c1956d3d302afc72fd9605c4f608b98
yuzuki517 <sgwr2532@outlook.com>
2026-02-01 01:29:48 +0900

    Update storage.py

--- config/storage.py
+++ config/storage.py
@@ -1,26 +1,14 @@
 from storages.backends.azure_storage import AzureStorage
+from django.conf import settings
+
 
 class AzureStaticStorage(AzureStorage):
-    account_name = None 
-    account_key = None
-    azure_container = None
-    expiration_secs = None
-    def __init__(self, *args, **kwargs):
-        from django.conf import settings
-        self.account_name = settings.AZURE_ACCOUNT_NAME
-        self.account_key = settings.AZURE_ACCOUNT_KEY
-        self.azure_container = settings.AZURE_CONTAINER_STATIC
-        super().__init__(*args, **kwargs)
+    account_name = settings.AZURE_ACCOUNT_NAME
+    account_key = settings.AZURE_ACCOUNT_KEY
+    azure_container_name = settings.AZURE_CONTAINER_STATIC
+
 
 class AzureMediaStorage(AzureStorage):
-    account_name = None
-    account_key = None
-    azure_container = None
-    expiration_secs = None
-
-    def __init__(self, *args, **kwargs):
-        from django.conf import settings
-        self.account_name = settings.AZURE_ACCOUNT_NAME
-        self.account_key = settings.AZURE_ACCOUNT_KEY
-        self.azure_container = settings.AZURE_CONTAINER_MEDIA
-        super().__init__(*args, **kwargs)
+    account_name = settings.AZURE_ACCOUNT_NAME
+    account_key = settings.AZURE_ACCOUNT_KEY
+    azure_container_name = settings.AZURE_CONTAINER_MEDIA
