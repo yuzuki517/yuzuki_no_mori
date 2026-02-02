7dd352282bcb8b4407438c347e6b00965d23a365
yuzuki517 <sgwr2532@outlook.com>
2026-02-03 01:17:39 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
