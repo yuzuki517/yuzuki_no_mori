0a3e7db31a0f44d603ab98ebb4b54251c54c45eb
yuzuki517 <sgwr2532@outlook.com>
2026-02-14 03:47:45 +0900

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
     path('category/<slug:slug>/', views.CategoryListView.as_view(), name='category_posts'), 
     path('tag/<slug:slug>/', views.TagListView.as_view(), name='tag_posts'), 
     path('archive/<int:year>/<int:month>/', views.ArchiveListView.as_view(), name='archive_posts'), 
