6e206abd6dc43bae2aadff09243d4e6fe42685f6
yuzuki517 <sgwr2532@outlook.com>
2026-02-23 18:37:44 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,4 +10,4 @@ python manage.py collectstatic --noinput
 # Gunicorn でアプリを起動
 # Azure App Service では $PORT が割り当てられるためそれを使う
 : "${PORT:=8000}"
-exec gunicorn --bind 0.0.0.0:${PORT} mysite.wsgi:application
+exec gunicorn --bind 0.0.0.0:${PORT} config.wsgi:application
